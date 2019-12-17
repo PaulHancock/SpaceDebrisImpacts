16d4243b9334eb9eaad219ced36fa368416feb40
PaulHancock <paul.hancock@curtin.edu.au>
2019-12-17 10:39:27 +0800

    auto update ACMA data

---
+++ data/README.md
@@ -0,0 +1,6 @@
+ACMA
+----
+
+Data downloaded from `https://www.acma.gov.au/radiocomms-licence-data` on 27/11/2019.
+
+
---
+++ data/getACMA.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+cd ACMA
+wget http://web.acma.gov.au/rrl-updates/spectra_licence_hrp.zip
+gunzip spectra_licence_hrp.zip
+wget http://web.acma.gov.au/rrl-updates/spectra_rrl.zip
+gunzip spectra_rrl.zip 
